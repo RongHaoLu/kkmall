a6a586a602bc27a4f39811d53135b9d39f949a89
RongHaoLU <1796471427@qq.com>
2022-08-13 15:56:16 +0800

    项目初始结构创建

---
+++ kkmalll-member/src/main/java/com/rh/kkmall/member/KkmalllMemberApplication.java
@@ -0,0 +1,13 @@
+package com.rh.kkmall.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkmalllMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkmalllMemberApplication.class, args);
+    }
+
+}
---
+++ kkmalll-member/src/test/java/com/rh/kkmall/member/KkmalllMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rh.kkmall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkmalllMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
